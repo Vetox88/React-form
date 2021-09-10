830ae0373557a5756180602c450127f149ac1b5b
vetox88 <sb.sami.bes@gmail.com>
2021-09-10 23:08:40 +0200

    adding logo and sign up button

---
+++ src/components/LogoContainer.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components'
+
+export const LogoContainer = styled.div`
+border-radius: 10px 0 0 10px;
+background: #000;
+display: flex;
+    align-content: center;
+    justify-content: center;
+}
+`
